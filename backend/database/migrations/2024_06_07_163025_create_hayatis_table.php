6fe7570c03d34a4221d68ceb0b8eda6326b6aed5
05Kass <kasnadi@teknokrat.ac.id>
2024-06-07 23:40:00 +0700

    master :ganti nama table

--- backend/database/migrations/2024_06_07_163025_create_hayatis_table.php
+++ backend/database/migrations/2024_06_07_163025_create_hayatis_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('hayatis', function (Blueprint $table) {
+        Schema::create('hayati', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
             $table->string('jenis');
@@ -26,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('hayatis');
+        Schema::dropIfExists('hayati');
     }
 };
