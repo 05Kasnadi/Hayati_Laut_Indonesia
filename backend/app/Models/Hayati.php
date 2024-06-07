a8441943916d949b90d5220a74caf326accac4bd
05Kass <kasnadi@teknokrat.ac.id>
2024-06-07 23:34:37 +0700

    master :buat protedted table

--- backend/app/Models/Hayati.php
+++ backend/app/Models/Hayati.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Hayati extends Model
 {
     use HasFactory;
+    protected $table ="hayati";
+    protected $fillable =['nama','jenis','lokalitas','deskripsi'];
 }
